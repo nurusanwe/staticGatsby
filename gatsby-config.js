2ff7aa699af9a4b0f73401b6a44123a0d5fea356
RGT <rgmorinehtar@gmail.com>
2018-07-07 18:59:08 +0200

    Refacto code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,23 @@
-
 module.exports = {
-  siteMetadata: {
-    title: `Happy pandas`,
-    postTitle: `Here are some posts`,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
-    },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-glamor`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-  ],
+	siteMetadata: {
+		title: `Happy pandas`,
+		postTitle: `Here are some posts`
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `src`,
+				path: `${__dirname}/src/`
+			}
+		},
+		`gatsby-transformer-remark`,
+		`gatsby-plugin-glamor`,
+		{
+			resolve: `gatsby-plugin-typography`,
+			options: {
+				pathToConfigModule: `src/utils/typography.js`
+			}
+		}
+	]
 };
-
